02d33834877c2df2d302d719e34f8f33acb383df
jugglingcats <github@akirkpatrick.com>
2021-11-12 12:47:34 +0000

    Mobile top nav

--- src/framework/nav/TopNav.tsx
+++ src/framework/nav/TopNav.tsx
@@ -2,12 +2,13 @@ import { useRootNav } from "../providers/NavProvider"
 import { Link, useLocation } from "react-router-dom"
 import { Menu, Space } from "antd"
 import * as React from "react"
+import { useState } from "react"
 import styled from "@emotion/styled"
 
 import { ReactComponent as StandardLogo } from "../../images/logos/small-logo.svg?inline"
 import { ReactComponent as SmallLogo } from "../../images/logos/tiny-logo.svg?inline"
 
-import { GithubOutlined, MenuOutlined, YoutubeOutlined } from "@ant-design/icons"
+import { CloseOutlined, GithubOutlined, MenuOutlined, YoutubeOutlined } from "@ant-design/icons"
 import { Section } from "../components/Section"
 
 const StyledTopNav = styled.div`
@@ -40,7 +41,9 @@ const StyledTopNav = styled.div`
         }
 
         .nav-narrow {
-            display: block;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
             text-align: right;
             font-size: 1.5em;
             cursor: pointer;
@@ -71,7 +74,7 @@ const StyledTopNav = styled.div`
 
 // doesn't work when placed in component above, I think because menu items are created dynamically
 const StyledMenuItem = styled(Menu.Item)`
-    &&& {
+    &.horizontal {
         padding: 8px 12px;
         height: auto;
         line-height: 20px;
@@ -94,56 +97,72 @@ const StyledMenuItem = styled(Menu.Item)`
     }
 `
 
-export const TopNav = () => {
+const NavMenu = ({ mode }) => {
     const { pathname } = useLocation()
     const nav = useRootNav()
+    const ancestors = pathname.split("/").map((_, index, path) => path.slice(0, index).join("/"))
+
+    return (
+        <Menu mode={mode} defaultOpenKeys={ancestors} selectedKeys={[pathname]}>
+            {nav.children.map(({ path, title, children }) =>
+                children?.length ? (
+                    <Menu.SubMenu key={path} title={title} popupOffset={[0, 20]}>
+                        {children.map(({ path, title, subtitle, children }) => {
+                            const to = children.length ? children[0].path : path
+                            return (
+                                <StyledMenuItem key={path} className={mode}>
+                                    <Link to={to}>
+                                        <div className="title">{title}</div>
+                                        <div className="subtitle">{subtitle}</div>
+                                    </Link>
+                                </StyledMenuItem>
+                            )
+                        })}
+                    </Menu.SubMenu>
+                ) : (
+                    <Menu.SubMenu key={path}>NO CHILDREN</Menu.SubMenu>
+                )
+            )}
+        </Menu>
+    )
+}
+
+export const TopNav = () => {
+    const [showMenu, setShowMenu] = useState(false)
 
     // we are expecting each node in the top nav to have children
 
     return (
-        <Section background={"BackgroundGrey"} guttered>
-            <StyledTopNav>
-                <div className="nav-wide">
-                    <Link className="logo" to="/">
-                        <StandardLogo height={"43px"} width={"139px"} />
-                    </Link>
-                    <Menu mode="horizontal" selectedKeys={[pathname]} forceSubMenuRender>
-                        {nav.children.map(({ path, title, children }) =>
-                            children?.length ? (
-                                <Menu.SubMenu key={path} title={title} popupOffset={[0, 20]}>
-                                    {children.map(({ path, title, subtitle, children }) => {
-                                        const to = children.length ? children[0].path : path
-                                        return (
-                                            <StyledMenuItem key={path}>
-                                                <Link to={to}>
-                                                    <div className="title">{title}</div>
-                                                    <div className="subtitle">{subtitle}</div>
-                                                </Link>
-                                            </StyledMenuItem>
-                                        )
-                                    })}
-                                </Menu.SubMenu>
-                            ) : (
-                                <Menu.SubMenu key={path}>NO CHILDREN</Menu.SubMenu>
-                            )
+        <>
+            <Section background={"BackgroundGrey"} guttered>
+                <StyledTopNav>
+                    <div className="nav-wide">
+                        <Link className="logo" to="/">
+                            <StandardLogo height={"43px"} width={"139px"} />
+                        </Link>
+                        <NavMenu mode="horizontal" />
+                        <Space size="middle">
+                            <a href={"https://www.github.com/glowbuzzer"}>
+                                <GithubOutlined style={{ fontSize: "24px", color: "#9254de" }} />
+                            </a>
+                            <a href={"https://www.youtube.com/channel/UCd5lSqWK5Ep4su1sHx6kkUA"}>
+                                <YoutubeOutlined style={{ fontSize: "24px", color: "#9254de" }} />
+                            </a>
+                        </Space>
+                    </div>
+                    <div className="nav-narrow">
+                        <Link className="logo" to="/">
+                            <SmallLogo height={"43px"} width={"43px"} />
+                        </Link>
+                        {showMenu ? (
+                            <CloseOutlined onClick={() => setShowMenu(false)} />
+                        ) : (
+                            <MenuOutlined onClick={() => setShowMenu(true)} />
                         )}
-                    </Menu>
-                    <Space size="middle">
-                        <a href={"https://www.github.com/glowbuzzer"}>
-                            <GithubOutlined style={{ fontSize: "24px", color: "#9254de" }} />
-                        </a>
-                        <a href={"https://www.youtube.com/channel/UCd5lSqWK5Ep4su1sHx6kkUA"}>
-                            <YoutubeOutlined style={{ fontSize: "24px", color: "#9254de" }} />
-                        </a>
-                    </Space>
-                </div>
-                <div className="nav-narrow">
-                    <a href="/">
-                        <SmallLogo height={"43px"} width={"43px"} />
-                    </a>
-                    <MenuOutlined onClick={() => alert("todo")} />
-                </div>
-            </StyledTopNav>
-        </Section>
+                    </div>
+                </StyledTopNav>
+            </Section>
+            <Section>{showMenu && <NavMenu mode="inline" />}</Section>
+        </>
     )
 }
--- src/framework/providers/NavProvider.tsx
+++ src/framework/providers/NavProvider.tsx
@@ -10,8 +10,8 @@
 import { createContext, FC } from "react"
 
 // @ts-ignore because globEager is not defined
-const imports = import.meta.globEager("../../pages/**/*.{jsx,tsx,md,mdx,ts}")
-// const imports = import.meta.globEager("../../pages/playground/**/*.{jsx,tsx,md,mdx,ts}")
+// const imports = import.meta.globEager("../../pages/**/*.{jsx,tsx,md,mdx,ts}")
+const imports = import.meta.globEager("../../pages/playground/**/*.{jsx,tsx,md,mdx,ts}")
 // const imports = import.meta.globEager("../../.test/**/*.{jsx,tsx,md,mdx,ts}")
 // console.log("IMPORTS", imports)
 
