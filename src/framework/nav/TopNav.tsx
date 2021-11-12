797ec41e5b465c5dc2cf92f0071538d9a02ca83c
jugglingcats <github@akirkpatrick.com>
2021-11-12 14:16:51 +0000

    More work on top nav

--- src/framework/nav/TopNav.tsx
+++ src/framework/nav/TopNav.tsx
@@ -17,22 +17,44 @@ const StyledTopNav = styled.div`
         padding-top: 10px;
     }
 
-    .ant-menu {
+    .ant-menu-horizontal {
+        display: inline-block;
+        text-align: right;
+        background: none;
+        border: none;
         padding-top: 5px;
         flex-grow: 1;
         font-size: 1.1em;
+
+        .ant-menu-submenu {
+            padding-bottom: 10px !important;
+        }
+
+        .ant-menu-submenu {
+            margin: 0 12px !important;
+
+            @media (max-width: 1200px) {
+                margin: 0 4px !important;
+            }
+        }
+
+        .ant-menu-submenu-title {
+            margin: 0 12px;
+
+            @media (max-width: 1200px) {
+                margin: 0 4px;
+            }
+        }
     }
 
-    .ant-menu-submenu {
-        padding-bottom: 10px !important;
-    }
-
-    .nav-narrow {
+    .nav-narrow,
+    .nav-narrow-open {
         display: none;
     }
 
     .nav-wide {
         display: flex;
+        gap: 20px;
     }
 
     @media (max-width: 950px) {
@@ -48,28 +70,11 @@ const StyledTopNav = styled.div`
             font-size: 1.5em;
             cursor: pointer;
         }
-    }
 
-    &&& {
-        .ant-menu-submenu {
-            margin: 0 8px;
+        .nav-narrow-open {
+            display: block;
         }
     }
-
-    .ant-menu-submenu-title {
-        margin: 0 20px 0 20px;
-
-        @media (max-width: 1080px) {
-            margin: 0 8px 0 8px;
-        }
-    }
-
-    .ant-menu {
-        background: none;
-        border: none;
-    }
-
-    //height: 66px;
 `
 
 // doesn't work when placed in component above, I think because menu items are created dynamically
@@ -78,6 +83,11 @@ const StyledMenuItem = styled(Menu.Item)`
         padding: 8px 12px;
         height: auto;
         line-height: 20px;
+
+        @media (max-width: 950px) {
+            // avoids annoying flash of horizontal menu in mobile mode
+            display: none;
+        }
 
         a {
             color: grey;
@@ -100,17 +110,38 @@ const StyledMenuItem = styled(Menu.Item)`
 const NavMenu = ({ mode }) => {
     const { pathname } = useLocation()
     const nav = useRootNav()
-    const ancestors = pathname.split("/").map((_, index, path) => path.slice(0, index).join("/"))
+    const ancestors = pathname
+        .split("/")
+        .slice(1, -1)
+        .map((p, index, paths) => mode + ":/" + paths.slice(0, index + 1).join("/"))
+
+    const [openKeys, setOpenKeys] = React.useState(mode === "inline" ? ancestors : [])
+
+    const onOpenChange = keys => {
+        const last = keys[keys.length - 1]
+        setOpenKeys(last ? [last] : [])
+    }
+
+    // antd uses the key to construct an id, so we need to make them different based on mode, to avoid odd things happening
+    // (because we render this menu twice in different modes: inline on mobile and horizontal on desktop)
 
     return (
-        <Menu mode={mode} defaultOpenKeys={ancestors} selectedKeys={[pathname]}>
-            {nav.children.map(({ path, title, children }) =>
-                children?.length ? (
-                    <Menu.SubMenu key={path} title={title} popupOffset={[0, 20]}>
+        <Menu
+            key={mode}
+            className={mode}
+            mode={mode}
+            openKeys={openKeys}
+            defaultSelectedKeys={[...ancestors, mode + ":" + pathname]}
+            onOpenChange={onOpenChange}
+        >
+            {nav.children
+                .filter(node => node.children.length) // don't include nodes like 404 that have no children
+                .map(({ path, title, children }) => (
+                    <Menu.SubMenu key={mode + ":" + path} title={title} popupOffset={[0, 20]}>
                         {children.map(({ path, title, subtitle, children }) => {
                             const to = children.length ? children[0].path : path
                             return (
-                                <StyledMenuItem key={path} className={mode}>
+                                <StyledMenuItem key={mode + ":" + path} className={mode}>
                                     <Link to={to}>
                                         <div className="title">{title}</div>
                                         <div className="subtitle">{subtitle}</div>
@@ -119,10 +150,7 @@ const NavMenu = ({ mode }) => {
                             )
                         })}
                     </Menu.SubMenu>
-                ) : (
-                    <Menu.SubMenu key={path}>NO CHILDREN</Menu.SubMenu>
-                )
-            )}
+                ))}
         </Menu>
     )
 }
@@ -133,36 +161,40 @@ export const TopNav = () => {
     // we are expecting each node in the top nav to have children
 
     return (
-        <>
+        <StyledTopNav>
             <Section background={"BackgroundGrey"} guttered>
-                <StyledTopNav>
-                    <div className="nav-wide">
-                        <Link className="logo" to="/">
-                            <StandardLogo height={"43px"} width={"139px"} />
-                        </Link>
-                        <NavMenu mode="horizontal" />
-                        <Space size="middle">
-                            <a href={"https://www.github.com/glowbuzzer"}>
-                                <GithubOutlined style={{ fontSize: "24px", color: "#9254de" }} />
-                            </a>
-                            <a href={"https://www.youtube.com/channel/UCd5lSqWK5Ep4su1sHx6kkUA"}>
-                                <YoutubeOutlined style={{ fontSize: "24px", color: "#9254de" }} />
-                            </a>
-                        </Space>
-                    </div>
-                    <div className="nav-narrow">
-                        <Link className="logo" to="/">
-                            <SmallLogo height={"43px"} width={"43px"} />
-                        </Link>
-                        {showMenu ? (
-                            <CloseOutlined onClick={() => setShowMenu(false)} />
-                        ) : (
-                            <MenuOutlined onClick={() => setShowMenu(true)} />
-                        )}
-                    </div>
-                </StyledTopNav>
+                <div className="nav-wide">
+                    <Link className="logo" to="/">
+                        <StandardLogo height={"43px"} width={"139px"} />
+                    </Link>
+                    <NavMenu mode="horizontal" />
+                    <Space size="middle">
+                        <a href={"https://www.github.com/glowbuzzer"}>
+                            <GithubOutlined style={{ fontSize: "24px", color: "#9254de" }} />
+                        </a>
+                        <a href={"https://www.youtube.com/channel/UCd5lSqWK5Ep4su1sHx6kkUA"}>
+                            <YoutubeOutlined style={{ fontSize: "24px", color: "#9254de" }} />
+                        </a>
+                    </Space>
+                </div>
+                <div className="nav-narrow">
+                    <Link className="logo" to="/">
+                        <SmallLogo height={"43px"} width={"43px"} />
+                    </Link>
+                    {showMenu ? (
+                        <CloseOutlined onClick={() => setShowMenu(false)} />
+                    ) : (
+                        <MenuOutlined onClick={() => setShowMenu(true)} />
+                    )}
+                </div>
             </Section>
-            <Section>{showMenu && <NavMenu mode="inline" />}</Section>
-        </>
+            {showMenu && (
+                <Section background={"White"}>
+                    <div className="nav-narrow-open">
+                        <NavMenu mode="inline" />
+                    </div>
+                </Section>
+            )}
+        </StyledTopNav>
     )
 }
