14ba922768960e90e216a4d31d89f15b054d7236
jugglingcats <github@akirkpatrick.com>
2021-12-03 17:01:57 +0000

    Fix deploy

--- deploy-site.sh
+++ deploy-site.sh
@@ -9,5 +9,5 @@ EOF
 
 export AWS_PROFILE=gb-deploy
 
-aws s3 sync --quiet --delete --acl public-read ./root/$1/dist "s3://$2" && \
+aws s3 sync --delete --acl public-read ./root/$1/dist "s3://$2" && \
 aws cloudfront create-invalidation --paths "/*" --distribution-id $3
