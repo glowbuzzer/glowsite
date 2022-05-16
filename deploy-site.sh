b27574c655fa5f269a157de468afaad9acc0baa0
jugglingcats <github@akirkpatrick.com>
2022-05-16 19:31:02 +0100

    Don't delete releases when running deploy-site.sh - attempt 2

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 sync --delete ./root/$1/dist "s3://$2" --exclude releases && \
+aws s3 sync --delete ./root/$1/dist "s3://$2" --include "index.html" --include "assets/*" --exclude "releases/*" && \
 aws cloudfront create-invalidation --paths "/*" --distribution-id $3
