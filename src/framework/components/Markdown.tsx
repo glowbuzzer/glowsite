e808e4547684bbd1539624aef24b938176f211c6
jugglingcats <github@akirkpatrick.com>
2022-06-07 15:53:14 +0100

    Fix more issues

--- src/framework/components/Markdown.tsx
+++ src/framework/components/Markdown.tsx
@@ -4,8 +4,8 @@ import ReactMarkdown from "react-markdown"
 import { typedocLinkify } from "./typedocLinkify"
 // @ts-ignore not sure why but IDEA doesn't like this reference but vite is happy
 import { remarkEntities } from "../../../plugins/remark-entities.mjs"
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {tomorrow} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 export const Markdown = props => {
     return (
@@ -13,12 +13,12 @@ export const Markdown = props => {
             {...props}
             remarkPlugins={[typedocLinkify, remarkEntities]}
             components={{
-                a: ({ node, href, ref, ...rest }) => <Link to={href} {...rest} />,
-                code({node, inline, className, children, ...props}) {
-                    const match = /language-(\w+)/.exec(className || '')
+                a: ({ node, href, ...rest }) => <Link to={href} {...rest} />,
+                code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "")
                     return !inline && match ? (
                         <SyntaxHighlighter
-                            children={String(children).replace(/\n$/, '')}
+                            children={String(children).replace(/\n$/, "")}
                             style={tomorrow}
                             language={match[1]}
                             PreTag="div"
