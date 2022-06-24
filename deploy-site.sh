2990122c45a10e0bbd363b67f7dfe8c6b3fbc858
jugglingcats <github@akirkpatrick.com>
2022-06-24 12:08:49 +0100

    chore: add region to aws commands

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 sync --delete ./root/$1/dist "s3://$2" --include "index.html" --include "assets/*" --exclude "releases/*" --exclude "examples/*" --exclude "gbr/*" && \
-aws cloudfront create-invalidation --paths "/*" --distribution-id $3
+aws s3 sync --region eu-west-1 --delete ./root/$1/dist "s3://$2" --include "index.html" --include "assets/*" --exclude "releases/*" --exclude "examples/*" --exclude "gbr/*" && \
+aws cloudfront create-invalidation --region eu-west-1 --paths "/*" --distribution-id $3
