6a47fd5ca7867fd6b30ea5fc520106dcf1212f77
jugglingcats <github@akirkpatrick.com>
2023-01-17 11:58:02 +0000

    fix: support external links in LandingPageLink.tsx

--- src/framework/conversions/LandingPageLink.tsx
+++ src/framework/conversions/LandingPageLink.tsx
@@ -5,10 +5,28 @@ declare const gtag
 
 export const LandingPageLink = ({ children, ...props }: LinkProps) => {
     function click() {
-        console.log("landing page link click")
         gtag("event", "exit_landing", {
             event_label: props.to
         })
+    }
+
+    function click_href() {
+        try {
+            const url = new URL(props.to as string)
+            gtag("event", "exit_landing", {
+                event_label: url.hostname
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    if (typeof props.to === "string" && props.to.startsWith("http")) {
+        return (
+            <a href={props.to} onClick={click_href}>
+                {children}
+            </a>
+        )
     }
 
     return (
