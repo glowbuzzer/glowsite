0c27b04a138f51e83083bc9f2690441253e85251
jugglingcats <github@akirkpatrick.com>
2023-06-12 09:06:57 +0100

    chore: update gbc and gbr versions

--- src/versions.mjs
+++ src/versions.mjs
@@ -1,6 +1,6 @@
 export const LATEST_VERSIONS={
-    gbc_version: "v1.4.1",
-    gbr_version: "v1.7.3",
+    gbc_version: "v1.5.0",
+    gbr_version: "v1.8.0",
     gbem_version: "v1.1.0",
     gbsm_version: "v1.0.0"
 }
